58a7fbbc5846525a8cc53bc877f3c64353bd6009
Mike Sarahan <msarahan@gmail.com>
2022-08-17 17:28:43 -0500

    fix win paths in repack

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
-robocopy /E "%SRC_DIR%\bin" "%PREFIX%"
+robocopy /E "%SRC_DIR%\bin" %LIBRARY_BIN%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
-robocopy /E "%SRC_DIR%\lib" "%PREFIX%"
+robocopy /E "%SRC_DIR%\lib" "%LIBRARY_LIB%"
 if %ERRORLEVEL% GEQ 8 exit 1
 
-robocopy /E "%SRC_DIR%\include" "%PREFIX%"
+robocopy /E "%SRC_DIR%\include" "%LIBRARY_INC%"
 if %ERRORLEVEL% GEQ 8 exit 1
